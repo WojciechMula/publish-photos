7f8da20045f5f1df9f00b9ebe9e80db9d60b4ea2
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2026-01-21 00:03:25 +0100

    gui: update error list view

--- crates/gui/src/modal_errors.rs
+++ crates/gui/src/modal_errors.rs
@@ -9,12 +9,14 @@ use crate::modal::ModalWindowTrait;
 use crate::style::Style;
 use const_format::formatcp as fmt;
 use db::Database;
+use db::PostId;
 use egui::Button;
 use egui::Key;
 use egui::Label;
 use egui::ScrollArea;
 use egui::Ui;
 use graphapi::manager::SocialMediaErrorList;
+use graphapi::SocialMediaError;
 
 use egui_material_icons::icons::ICON_REPLAY;
 
@@ -45,39 +47,7 @@ impl ModalWindowTrait for ModalErrors {
     ) {
         ScrollArea::vertical().show(ui, |ui| {
             for (id, errors) in &self.errors {
-                ui.horizontal(|ui| {
-                    ui.horizontal(|ui| {
-                        let post = db.post(id);
-                        add_image(
-                            ui,
-                            &post.files[0],
-                            image_cache,
-                            style.image.thumbnail_width,
-                            style.image.radius,
-                        );
-                    });
-
-                    ui.vertical(|ui| {
-                        let button =
-                            Button::new(fmt!("{ICON_REPLAY} Retry")).fill(style.button.publish);
-                        if ui.add(button).clicked() {
-                            queue.push_back(Message::StartPublishing(*id));
-                        }
-
-                        for msg in errors {
-                            ui.horizontal(|ui| {
-                                if button::copy(ui, true) {
-                                    queue.push_back(Message::Copy(
-                                        ClipboardKind::Generic,
-                                        msg.clone(),
-                                    ));
-                                }
-                                let label = Label::new(msg).wrap();
-                                ui.add(label);
-                            });
-                        }
-                    });
-                });
+                show_errors(ui, id, errors, image_cache, style, db, queue);
             }
 
             ui.separator();
@@ -92,5 +62,88 @@ impl ModalWindowTrait for ModalErrors {
 
     fn keyboard_mapping(&self) -> &KeyboardMapping {
         &self.keyboard_mapping
+    }
+}
+
+fn show_errors(
+    ui: &mut Ui,
+    id: &PostId,
+    errors: &[SocialMediaError],
+    image_cache: &mut ImageCache,
+    style: &Style,
+    db: &Database,
+    queue: &mut MessageQueue,
+) {
+    ui.horizontal(|ui| {
+        ui.horizontal(|ui| {
+            let post = db.post(id);
+            add_image(
+                ui,
+                &post.files[0],
+                image_cache,
+                style.image.thumbnail_width,
+                style.image.radius,
+            );
+        });
+
+        ui.vertical(|ui| {
+            let button = Button::new(fmt!("{ICON_REPLAY} Retry")).fill(style.button.publish);
+            if ui.add(button).clicked() {
+                queue.push_back(Message::StartPublishing(*id));
+            }
+
+            for err in errors {
+                ui.horizontal(|ui| {
+                    if button::copy(ui, true) {
+                        queue.push_back(Message::Copy(ClipboardKind::Generic, err2string(err)));
+                    }
+
+                    fn add_label(ui: &mut Ui, label: &String) {
+                        let label = Label::new(label).wrap();
+                        ui.add(label);
+                    }
+
+                    match err {
+                        SocialMediaError::String(msg) => {
+                            add_label(ui, msg);
+                        }
+                        SocialMediaError::FacebookError(err) => {
+                            ui.vertical(|ui| {
+                                add_label(ui, &err.message);
+
+                                if let Some(msg) = &err.error_user_title {
+                                    add_label(ui, msg);
+                                }
+
+                                if let Some(msg) = &err.error_user_msg {
+                                    add_label(ui, msg);
+                                }
+
+                                add_label(ui, &err.url);
+                            });
+                        }
+                    }
+                });
+            }
+        });
+    });
+}
+
+fn err2string(sme: &SocialMediaError) -> String {
+    match sme {
+        SocialMediaError::String(msg) => msg.clone(),
+        SocialMediaError::FacebookError(fb) => {
+            let mut multiline = String::new();
+            multiline += &format!("{}\n", fb.message);
+            if let Some(msg) = &fb.error_user_title {
+                multiline += &format!("{msg}\n");
+            }
+            if let Some(msg) = &fb.error_user_msg {
+                multiline += &format!("{msg}\n");
+            }
+            multiline += &fb.url;
+
+            multiline
+        }
     }
 }
